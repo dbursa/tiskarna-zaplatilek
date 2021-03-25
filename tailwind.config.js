0a53a254af7962b4be90b0da5f2db8ce6c098664
dbursa <danielbursa93@gmail.com>
2021-03-25 18:08:29 +0100

    final release

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  purge: [],
+  purge: [
+    'src/Templates/404.html',
+    'src/Templates/form.html',
+    'src/Templates/index.html',
+    'src/Templates/kontakt.html',
+    'src/Templates/reference.html',
+    'src/Templates/layouts/layout.html',
+
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
